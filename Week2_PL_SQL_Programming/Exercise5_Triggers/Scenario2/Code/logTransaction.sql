8b1c39555fe0ebb25893e16e4c988101acdb4242
Achyut Nangunoori <136007932+AchyutNangunoori@users.noreply.github.com>
2025-06-30 00:40:06 +0530

    Add files via upload

---
+++ Week2_PL_SQL_Programming/Exercise5_Triggers/Scenario2/Code/logTransaction.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE TRIGGER LogTransaction
+AFTER INSERT ON Transactions
+FOR EACH ROW
+BEGIN
+INSERT INTO AuditLog (TransactionID, AccountID, TransactionDate, Amount, TransactionType, LogTimestamp) VALUES (:NEW.TransactionID, :NEW.AccountID, :NEW.TransactionDate,:NEW.Amount, :NEW.TransactionType, SYSDATE);
+END;
+/
