cc079b69cfb102267bc6a116344ed94e52b0ff0c
Achyut Nangunoori <136007932+AchyutNangunoori@users.noreply.github.com>
2025-06-30 00:11:00 +0530

    Add files via upload

---
+++ Week2_JUNIT_MOCKITO_SLF4J/SL4J_Logging_Exercises/Exercise3/Code/Exercise3/src/main/java/SLF4JExercise3/App.java
@@ -0,0 +1,13 @@
+package SLF4JExercise3;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
