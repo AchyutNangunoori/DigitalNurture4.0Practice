49ffe410035379b462b06d9c0785f5fa476e2bb3
Achyut Nangunoori <136007932+AchyutNangunoori@users.noreply.github.com>
2025-07-27 00:36:57 +0530

    Add files via upload

---
+++ Week6_Week7_React/2.ReactJS - HOL/studentapp/src/Components/Home.js
@@ -0,0 +1,10 @@
+import React,{Component} from "react";
+
+export class Home extends Component{
+    render(){
+        return(
+        <div>
+            <h3>Welcome to the homepage of student managemnet portal</h3>
+        </div>)
+    }
+}
